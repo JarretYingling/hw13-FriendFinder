730544cd09aa3a10e63c32db0fba5f4107d25882
Jarret Yingling <44506154+JarretYingling@users.noreply.github.com>
2019-02-23 23:17:27 -0500

    readme and tweaks to links

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,15 +8,9 @@ const compareFriends = require("../data/friends.js").compareFriends;
 
 module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
-        if (error) {
-            res.status(500).send("server error");
-        }
-        res.json(`get:\n${friendsArray}\n`);
+        res.json(friendsArray);
     });
     app.post("/api/friends", function (req, res) {
-        if (error) {
-            res.status(500).send("server error");
-        }
         let match = compareFriends(req.body.scores);
         friendsArray.push(req.body);
         res.json(friendsArray[match]);
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,16 +5,10 @@ const path = require("path");
 
 module.exports = function (app) {
     app.get('/survey', function (req, res) {
-        if (error) {
-            res.status(500).send("server error");
-        }
         res.sendFile(path.join(__dirname + "/../public/survey.html"));
     });
 
     app.use(function (req, res) {
-        if (error) {
-            res.status(500).send("server error");
-        }
         res.sendFile(path.join(__dirname + "/../public/home.html"));
     });
 }
